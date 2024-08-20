b104b451c525e1265662b3bcc1f70737a6dd8360
Ahademy Code <ahademycode@Edwins-MacBook-Pro.local>
2024-08-20 17:17:52 +0700

    Base project

---
+++ 1.lexical-environment/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function createCounter(initialValue) {
+  // start coding here
+}
+
+const counter = createCounter(10);
+counter.increment(5);
+counter.decrement(3);
+counter.reset();
+console.log(counter.getValue()); // 10
+console.log(counter.getLogs()); // // this should produce the following:
+// [
+//   "Incremented by 5, new value: 15",
+//   "Decremented by 3, new value: 12",
+//   "Counter reset to initial value: 10"
+// ]
